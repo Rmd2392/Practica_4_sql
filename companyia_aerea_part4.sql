59f8d8d5363c5b10416c57d0703867f158c9e8de
Rmd2392 <rmd9292@gmail.com>
2025-03-05 08:35:49 +0100

    pregunta 1

--- companyia_aerea_part4.sql
+++ companyia_aerea_part4.sql
@@ -8,17 +8,18 @@
 
 -- Pregunta 1
 
-SELECT a.ciutat, ao.nom AS aeroport_origen, v.data, v.codi
-FROM vols v
-JOIN aeroports ao ON v.id_aeroport_origen = ao.id
-JOIN aeroports a ON ao.id_ciutat = a.id
-WHERE v.durada < 40
-  AND MONTH(v.data) = 2 AND YEAR(v.data) = 2024
-  AND v.estat = 'delayed'
-ORDER BY a.ciutat, v.data;
+SELECT aeroport.ciutat, aeroport.nom as aeroport, vol.data, vol.codi
+FROM vol
+  JOIN aeroport on (aeroport.codi=vol.aeroport_origen)
+WHERE durada < 40 and MONTH(data)= 2 and year(data) = 2024 and descripcio = 'DELAYED' order by ciutat, data;
 
 -- Pregunta 2
-select 'No ho sé';
+select any_fabricacio as 'any', companyia as 'companyia', num_serie, tipus
+from avio
+  join companyia on companyia.nom = avio.companyia
+where companyia.pais like 'spain'
+  and any_fabricacio < 2000
+order by any_fabricacio desc, companyia asc, num_serie asc;
 
 -- Pregunta 3
 select 'No ho sé';
